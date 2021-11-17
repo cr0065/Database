1915895a0cd298552db27fe2ba2219cb35ff1906
cr0065 <cr0064@uah.edu>
2021-11-16 19:18:03 -0600

    Just Baseline connection status of a database (Dummy Info used).

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.sql.*;
+
 public class Main {
+    static final String DatabaseURL = "Database URL would go";
+    static final String User = "Username";
+    static final String Password = "Password";
+    static final String Query = "SELECT id, first, last, age FROM Employees";
 
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+        // Open your connection here
+        try (Connection connect = DriverManager.getConnection(DatabaseURL, User, Password)){
+            Statement statement = connect.createStatement();
+            ResultSet results = statement.executeQuery(Query);
+
+            while (results.next()) {
+                // Retrieve Information by column name
+                System.out.print("ID: " + results.getInt("id"));
+                System.out.print(", Age: " + results.getInt("age"));
+                System.out.print(", First: " + results.getString("first"));
+                System.out.println(", Last: " + results.getString("last"));
+            }
+        } catch (SQLException throwables) {
+        throwables.printStackTrace();
+        }
     }
 }
