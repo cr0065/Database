c3ebd8e8838350b32c17d0bd0f1ebeda3cced945
cr0065 <71351420+cr0065@users.noreply.github.com>
2021-11-18 09:48:13 -0800

    Add files via upload
    New python File

--- main.py
+++ main.py
@@ -3,19 +3,37 @@ import sqlite3
 try:
     connection = sqlite3.connect('C:/Users/aceth/Desktop/App.db') # Path to the database file to edit.
 
-    AddQuery = '''Code to add shit''' # insert code here in the morning
+    #AddQuery = '''INSERT INTO Users (Username, Email, Pass, Birthday, createdDateTime)
+    #      VALUES ('acethegenie', 'SomeEmail@something.com', 'RanDomPasSW0RD1234!', '1999-02-16', '');'''
 
-    UpdateQuery = '''Code to Update Shit''' # insert code here in the morning
+    #UpdateQuery = '''Update Users set createdDateTime = '2021-11-18'
+    #                WHERE Username = 'acethegenie';''' # insert code here in the morning
 
-    DeleteQuery = '''Code to Delete Shit ''' # insert code here in the morning
+    #DeleteQuery = '''DELETE from Users
+    #                    where Username = 'ironspider';''' # insert code here in the morning
+
+    #SelectQuery = '''SELECT * FROM Users'''
 
     connect_cursor = connection.cursor()
 
-    connect_cursor.execute(AddQuery)
-    connect_cursor.execute(UpdateQuery)
-    connect_cursor.execute(DeleteQuery)
+    buffer = ""
 
-    connection.commit()
+    while True:
+        line = input("Input an SQL Command: ")
+        if line == "":
+            break
+        buffer += line
+        try:
+            connect_cursor.execute(buffer)
+            connection.commit()
+
+            if buffer.startswith("SELECT"):
+                connect_cursor.execute(buffer)
+                for row in connect_cursor.fetchall():
+                    print(row)
+        except sqlite3.Error as e:
+            print("An error occurred: ", e.args[0])
+        buffer = ""
 
     print("Updated the Data within the .db file")
 
